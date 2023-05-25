993896248e0e2c46e3263368eb1e8e23a256239b
skbharti1 <saurabhkrbharti@gmail.com>
2023-05-25 19:53:54 +0530

    small fix for branch coverage

--- src/utils/convertor.spec.js
+++ src/utils/convertor.spec.js
@@ -2,8 +2,8 @@ import { convert } from "./convertor";
 
 describe("Testing convertor", () => {
   it("should convert correctly", () => {
-    const data = [{ name: 'A', parent: '' }, { name: 'B', parent: 'A' }];
+    const data = [{ name: 'B', parent: 'A' }, { name: 'A', parent: '' }, { name: 'C', parent: 'A'}];
     const convertedData = convert(data);
-    expect(convertedData).toEqual([{ name: 'A', parent: '', children: [{ name: 'B', parent: 'A', children: [] }] }]);
+    expect(convertedData).toEqual([{ name: 'A', parent: '', children: [{ name: 'B', parent: 'A', children: [] }, { name: 'C', parent: 'A', children: [] }] }]);
   });
 });
